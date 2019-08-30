bd606c7700b6e2aaaa6784148631aba0ff94a4fa
Cxx0822 <1556464090@qq.com>
2019-08-30 10:11:05 +0800

    Main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "classifierTrain.h"
+
+int main()
+{
+	clock_t start, end;
+	start = clock();
+	ClassifierTrain train;
+	//train.cal_pos_vector("img_train_pos/*.jpg", "label_train_pos/*.xml", "data_1.txt");
+	//train.cal_neg_vector("img_train_neg/*.jpg", "data_2.txt");
+	train.result_test("img_test/*.jpg");
+	end = clock();
+	double endtime = (double)(end - start) / CLOCKS_PER_SEC;
+	cout << "Total time:" << endtime << endl;		//sÎªµ¥Î»
+	
+	return 0;
+}
